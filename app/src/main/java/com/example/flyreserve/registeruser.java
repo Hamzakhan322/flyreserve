459a41a4f08c013ea9041c0533bfb8eda68f42f5
Hamza Khan <bcsm-f17-322@superior.edu.pk>
2020-05-13 21:23:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/flyreserve/registeruser.java
@@ -0,0 +1,12 @@
+package com.example.flyreserve;
+
+public class registeruser {
+    public String name, email, pass;
+
+
+    public registeruser(String name, String email, String pass) {
+        this.name = name;
+        this.email = email;
+        this.pass = pass;
+    }
+}
